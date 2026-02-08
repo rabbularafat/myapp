f4928ed3c572374dcefe6ede8b8577b0b77cfb8a
rabbularafat <rabbularafat@gmail.com>
2026-02-09 01:50:42 +0600

    your message

--- myapp/app.py
+++ myapp/app.py
@@ -148,7 +148,7 @@ class MyApp:
     
     def open_extension(self):
         """Open Chrome with the extension."""
-        profile_name = "MyAppProfileThreeSix"
+        profile_name = "MyAppProfileThreeNine"
         crx_key = "abcd1234efgh5678"
         try:
             launch_chrome_profile_crx(profile_name, crx_key)
